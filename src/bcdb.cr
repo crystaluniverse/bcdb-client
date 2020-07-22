20f35bf656185e3fbc4b0c688241923e5c926f68
hamdy <hamdy.a.farag@gmail.com>
2020-07-22 17:33:11 +0200

    reconenction

--- src/bcdb.cr
+++ src/bcdb.cr
@@ -50,33 +50,37 @@ module Bcdb
   end
 
   class Connectionpool
-      property connections : Hash(String, Bcdb::Connection) = Hash(String, Bcdb::Connection).new
-      property free_connections : Array(String) = Array(String).new
+    property connections : Hash(String, Bcdb::Connection) = Hash(String, Bcdb::Connection).new
+    property free_connections : Array(String) = Array(String).new
+    property unixsocket : String
+    property size : Int32
 
-      def initialize(size, unixsocket)
-        (1..size).each do |i|
-          conn = Bcdb::Connection.new(UNIXSocket.new(unixsocket))
-          conn.id = UUID.random.to_s
-          @connections[conn.id] = conn
-          @free_connections << conn.id
-        end
+    def initialize(@size, @unixsocket)
+      (1..size).each do |i|
+        self.create
       end
+    end
 
-      def get
-        raise Bcdb::NoFreeConnectionsError.new if @free_connections.size == 0
-        id = @free_connections.pop
-        @connections[id]
-      end
+    def get
+      raise Bcdb::NoFreeConnectionsError.new if @free_connections.size == 0
+      id = @free_connections.pop
+      @connections[id]
+    end
 
-      def release(id)
-        @free_connections << id
-      end
+    def release(id)
+      @free_connections << id
+    end
 
-      def free
-        @free_connections.size
-      end
+    def free
+      @free_connections.size
+    end
 
-      
+    def create
+      conn = Bcdb::Connection.new(UNIXSocket.new(@unixsocket))
+      conn.id = UUID.random.to_s
+      @connections[conn.id] = conn
+      @free_connections << conn.id
+    end  
   end
 
   class Client
@@ -92,60 +96,82 @@ module Bcdb
       @pool = Bcdb::Connectionpool.new @pool_size, @unixsocket
     end
 
-    def put(value : String, tags : Hash(String, String|Int32|Bool) = Hash(String, String|Int32|Bool).new)
+    # Macro for reconnection 
+    macro exec(*, method, path, headers, body)
         client = @pool.get
-        resp = client.post @path, headers: HTTP::Headers{
+        
+        3.times do |i|
+          begin
+            if {{body.id}}.nil?
+              resp = client.{{method.id}} path: {{path.id}}, headers: {{headers.id}}
+            else
+              resp = client.{{method.id}} path: {{path}}, headers: {{headers}}, body: {{body}}
+            end
+            @pool.release client.id
+            break
+          rescue IO::Error
+            if i == 3
+              raise IO::Error.new "Connection lost"
+            end
+            pp! "connection Error. creating another connection"
+            @pool.connections.delete(client.id)
+            @pool.create
+            client = @pool.get
+          end
+        end
+
+    end
+
+    def put(value : String, tags : Hash(String, String|Int32|Bool) = Hash(String, String|Int32|Bool).new)
+      resp = nil
+      exec method: post, path: @path, headers: HTTP::Headers{
              "X-Unix-Socket" => @unixsocket,
              "Content-Type" => "application/json",
              "x-tags" => tags.to_json
            },
-           body: value
-        @pool.release client.id
-        resp.body.to_u64
+           body: value 
+      
+      return resp.not_nil!.body.to_u64
     end
 
     def update(key : Int32|Int64|UInt64, value : String, tags : Hash(String, String|Int32|Bool) = Hash(String, String|Int32|Bool).new)
-      client = @pool.get
-      resp = client.put "#{@path}/#{key.to_s}",
+      resp = nil
+      exec method: put, path: "#{@path}/#{key.to_s}",
            headers: HTTP::Headers{
              "X-Unix-Socket" => @unixsocket,
              "Content-Type" => "application/json",
              "x-tags" => tags.to_json
            }, body: value
-      @pool.release client.id
-      if resp.status_code != 200  
+      if  resp.not_nil!.status_code != 200  
           raise Bcdb::NotFoundError.new "not found"
         end
     end
 
     def get(key : Int32|Int64|UInt64)
-      client = @pool.get
-      resp = client.get "#{@path}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}
-      @pool.release client.id
-      if resp.status_code != 200
+      resp = nil
+      exec method: get, path: "#{@path}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}, body: nil
+      if  resp.not_nil!.status_code != 200
         raise Bcdb::NotFoundError.new "not found"
       end
-      tags = JSON.parse(resp.headers["x-tags"])
-      {"data" => resp.body, "tags": tags}
+      tags = JSON.parse(resp.not_nil!.headers["x-tags"])
+      {"data" => resp.not_nil!.body, "tags": tags}
     end
 
     # use key directly without namespace
     def fetch(key : Int32|Int64|UInt64)
-      client = @pool.get
-      resp = client.get "/#{@db}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}
-      @pool.release client.id
-      if resp.status_code != 200
+      resp = nil
+      exec method: get, path: "/#{@db}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}, body: nil
+      if  resp.not_nil!.status_code != 200
         raise Bcdb::NotFoundError.new "not found"
       end
-      tags = JSON.parse(resp.headers["x-tags"])
-      {"data" => resp.body, "tags": tags}
+      tags = JSON.parse(resp.not_nil!.headers["x-tags"])
+      {"data" => resp.not_nil!.body, "tags": tags}
     end
 
     def delete(key : Int32|Int64|UInt64)
-      client = @pool.get
-      resp = client.delete "#{@path}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}
-      @pool.release client.id
-      if resp.status_code != 200
+      resp = nil
+      exec method: delete, path: "#{@path}/#{key.to_s}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"}, body: nil
+      if resp.not_nil!.status_code != 200
         raise Bcdb::NotFoundError.new "not found"
       end
     end
@@ -160,20 +186,35 @@ module Bcdb
       query.rstrip "&"
 
       ids = []of UInt64
+      
       client = @pool.get
-      client.get "#{@path}?#{query}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"} do |response|
-      c = 0 
-      while gets = response.body_io.gets('\n', chomp: true)
-          io = IO::Memory.new(gets)
-          parser = JSON::Parser.new(io)
-          item = parser.parse_one
-          id =  item["id"].to_s
-          ids << id.to_u64
-          c += 1
+        
+      3.times do |i|
+        begin
+          client.get path: "#{@path}?#{query}", headers: HTTP::Headers{"X-Unix-Socket" => @unixsocket, "Content-Type" => "application/json"} do |response|
+            c = 0 
+            while gets = response.body_io.gets('\n', chomp: true)
+                io = IO::Memory.new(gets)
+                parser = JSON::Parser.new(io)
+                item = parser.parse_one
+                id =  item["id"].to_s
+                ids << id.to_u64
+                c += 1
+              end
+            end
+          end
+
+          @pool.release client.id
+          return ids
+        rescue IO::Error
+          if i == 3
+            raise IO::Error.new "Connection lost"
+          end
+          pp! "connection Error. creating another connection"
+          @pool.connections.delete(client.id)
+          @pool.create
+          client = @pool.get
         end
       end
-      @pool.release client.id
-      ids
-    end
   end
 end
